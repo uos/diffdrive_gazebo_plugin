uos/diffdrive_gazebo_plugin

commit 66aab3722f947845a4b1828af8d53a67804a75c0

in-class initializer for static const double is invalid code
The c++ standard(s) do not permit in-class initializer of floating
point values (integral types + enums are fine...)
. I have no idea why this compiled before (gnu extension?).

The C++98 workaround for this is external initialization:
> gazebo::GazeboRosDiffdrive::CMD_VEL_TIMEOUT = 0.6;
The (preferred) C++11 solution is to use constexpr instead of const.

include/diffdrive_gazebo_plugin/gazebo_ros_diffdrive_uos.h
@@ -25,7 +25,7 @@ public:
 
 private:
   static const size_t NUM_JOINTS = 6;
-  static const double CMD_VEL_TIMEOUT = 0.6;
+  static constexpr double CMD_VEL_TIMEOUT = 0.6;
 
   void OnCmdVel(const geometry_msgs::TwistConstPtr &msg);
 

commit d9f6ab376fc2edd8b8c921b0baabe39500ed56a3

Replace c++11 constexpr with c++98 external initialization
This fixes the following compile error on Ubuntu 14.04 / Indigo:

    include/diffdrive_gazebo_plugin/gazebo_ros_diffdrive_uos.h:28:10: error: 'constexpr' does not name a type
       static constexpr double CMD_VEL_TIMEOUT = 0.6;

The problem is that 66aab37 introduced this C++11 feature, but 9edd4c8
replaced `add_compile_options(-std=c++11)` with `GAZEBO_CXX_FLAGS`.
However, `GAZEBO_CXX_FLAGS` is empty on Indigo (it works on Jade).

include/diffdrive_gazebo_plugin/gazebo_ros_diffdrive_uos.h
@@ -25,7 +25,7 @@ public:
 
 private:
   static const size_t NUM_JOINTS = 6;
-  static constexpr double CMD_VEL_TIMEOUT = 0.6;
+  static const double CMD_VEL_TIMEOUT;
 
   void OnCmdVel(const geometry_msgs::TwistConstPtr &msg);
 

src/gazebo_ros_diffdrive_uos.cpp
@@ -14,6 +14,8 @@ enum
   LEFT = 1, RIGHT = 4
 };
 
+const double gazebo::GazeboRosDiffdrive::CMD_VEL_TIMEOUT = 0.6;
+
 GazeboRosDiffdrive::GazeboRosDiffdrive() :
   wheel_speed_right_(0.0),
   wheel_speed_left_(0.0)