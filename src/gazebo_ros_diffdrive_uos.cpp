be2e9aeb3ea1088ecb021dab7adb9e6e1b9219fc
Sebastian Pütz <spuetz@uos.de>
2017-10-20 12:21:44 +0200

    four and six wheels support

--- src/gazebo_ros_diffdrive_uos.cpp
+++ src/gazebo_ros_diffdrive_uos.cpp
@@ -8,12 +8,6 @@
 
 using namespace gazebo;
 
-// index of left / right middle wheel joint
-enum
-{
-  LEFT = 1, RIGHT = 4
-};
-
 const double gazebo::GazeboRosDiffdrive::CMD_VEL_TIMEOUT = 0.6;
 
 GazeboRosDiffdrive::GazeboRosDiffdrive() :
@@ -21,11 +15,6 @@ GazeboRosDiffdrive::GazeboRosDiffdrive() :
   wheel_speed_left_(0.0)
 {
   this->spinner_thread_ = new boost::thread( boost::bind( &GazeboRosDiffdrive::spin, this) );
-
-  for (size_t i = 0; i < NUM_JOINTS; ++i)
-  {
-    joints_[i].reset();
-  }
 }
 
 GazeboRosDiffdrive::~GazeboRosDiffdrive()
@@ -67,41 +56,109 @@ void GazeboRosDiffdrive::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf )
   if (_sdf->HasElement("joint_states_topic_name"))
     joint_states_topic_name_ = _sdf->GetElement("joint_states_topic_name")->Get<std::string>();
 
-  js_.name.resize(NUM_JOINTS);
-  js_.position.resize(NUM_JOINTS);
-  js_.velocity.resize(NUM_JOINTS);
-  js_.effort.resize(NUM_JOINTS);
+  if (!ros::isInitialized())
+  {
+    int argc = 0;
+    char** argv = NULL;
+    ros::init(argc, argv, "gazebo_ros_diffdrive_uos", ros::init_options::NoSigintHandler|ros::init_options::AnonymousName);
+  }
 
-  for (size_t i = 0; i < NUM_JOINTS; ++i)
+  rosnode_ = new ros::NodeHandle(node_namespace_);
+
+  cmd_vel_sub_ = rosnode_->subscribe(cmd_vel_topic_name_, 1, &GazeboRosDiffdrive::OnCmdVel, this);
+  odom_pub_ = rosnode_->advertise<nav_msgs::Odometry> (odom_topic_name_, 1);
+  joint_state_pub_ = rosnode_->advertise<sensor_msgs::JointState> (joint_states_topic_name_, 1);
+
+  enum
+  {
+    LF = 0, // left front wheel
+    LM = 1, // left middle wheel
+    LR = 2, // left rear wheel
+    RF = 3, // right front wheel
+    RM = 4, // right middle wheel
+    RR = 5  // right rear wheel
+  };
+
+  std::vector<std::string> joint_names(6);
+
+  joint_names[LF] = "left_front_wheel_joint";
+  joint_names[LM] = "left_middle_wheel_joint";
+  joint_names[LR] = "left_rear_wheel_joint";
+  joint_names[RF] = "right_front_wheel_joint",
+  joint_names[RM] = "right_middle_wheel_joint";
+  joint_names[RR] = "right_rear_wheel_joint";
+
+  std::vector<physics::JointPtr> joints_tmp(6);
+
+  for(size_t i=0; i<joint_names.size(); i++)
+  {
+    // overwrite names, if configured in sdf
+    if(_sdf->HasElement(joint_names[i]))
+    {
+      joint_names[i] = _sdf->GetElement(joint_names[i])->Get<std::string>();
+    }
+
+    // get all joint objects, some of them might not exist in the model
+    joints_tmp[i] = my_parent_->GetJoint(joint_names[i]);
+  }
+
+  //check for availability
+
+  std::vector<int> missing_joints;
+
+  // left front, rear and right front and rear are required for a four wheel diffdrive robot
+  if(!joints_tmp[LF]) missing_joints.push_back(LF);
+  if(!joints_tmp[LR]) missing_joints.push_back(LR);
+  if(!joints_tmp[RF]) missing_joints.push_back(RF);
+  if(!joints_tmp[RR]) missing_joints.push_back(RR);
+
+  // if one of the middle joints is available than the other should be available, too.
+  if( !joints_tmp[LM] != !joints_tmp[RM] )
+  {
+    missing_joints.push_back(LM);
+    missing_joints.push_back(RM);
+  }
+
+  if(!missing_joints.empty())
+  {
+    // build helpful error message with the missing joints
+    std::string missing_err = "The controller couldn't get the joint(s): ";
+    for(size_t i=0; i<missing_joints.size(); i++)
+    {
+      missing_err += joint_names[missing_joints[i]] + (i != missing_joints.size()-1 ? ", " : "");
+    }
+    gzthrow("This plugin supports either a four or a six wheeled robot, due to that the middle wheels are optional. "
+                + missing_err);
+  }
+
+  // Determine the number of joints, the type of robot, either four wheeled or six wheeled.
+  bool six_wheeled = joints_tmp[LM] && joints_tmp[RM];
+  num_joints_ = six_wheeled ? 6 : 4;
+
+  int six_wheels[] = {LF, LM, LR, RF, RM, RR};
+  int four_wheels[] = {LF, LR, RF, RR};
+  int* wheels = six_wheeled ? six_wheels : four_wheels;
+
+  if ( six_wheeled ) ROS_INFO_STREAM("The robot is six wheeled.");
+  else ROS_INFO_STREAM("The robot is four wheeled");
+
+  left = six_wheeled ? 1 : 0;
+  right = six_wheeled ? 4 : 2;
+
+  js_.name.resize(num_joints_);
+  js_.position.resize(num_joints_);
+  js_.velocity.resize(num_joints_);
+  js_.effort.resize(num_joints_);
+  joints_.resize(num_joints_);
+
+  for (size_t i = 0; i < num_joints_; ++i)
   {
     js_.position[i] = 0;
     js_.velocity[i] = 0;
     js_.effort[i] = 0;
+    js_.name[i] = joint_names[wheels[i]];
+    joints_[i] = joints_tmp[wheels[i]];
   }
-
-  js_.name[0] = "left_front_wheel_joint";
-  if (_sdf->HasElement("left_front_wheel_joint"))
-    js_.name[0] = _sdf->GetElement("left_front_wheel_joint")->Get<std::string>();
-
-  js_.name[1] = "left_middle_wheel_joint";
-  if (_sdf->HasElement("left_middle_wheel_joint"))
-    js_.name[1] = _sdf->GetElement("left_middle_wheel_joint")->Get<std::string>();
-
-  js_.name[2] = "left_rear_wheel_joint";
-  if (_sdf->HasElement("left_rear_wheel_joint"))
-    js_.name[2] = _sdf->GetElement("left_rear_wheel_joint")->Get<std::string>();
-
-  js_.name[3] = "right_front_wheel_joint";
-  if (_sdf->HasElement("right_front_wheel_joint"))
-    js_.name[3] = _sdf->GetElement("right_front_wheel_joint")->Get<std::string>();
-
-  js_.name[4] = "right_middle_wheel_joint";
-  if (_sdf->HasElement("right_middle_wheel_joint"))
-    js_.name[5] = _sdf->GetElement("right_middle_wheel_joint")->Get<std::string>();
-
-  js_.name[5] = "right_rear_wheel_joint";
-  if (_sdf->HasElement("right_rear_wheel_joint"))
-    js_.name[5] = _sdf->GetElement("right_rear_wheel_joint")->Get<std::string>();
 
   wheel_sep_ = 0.34;
   if (_sdf->HasElement("wheel_separation"))
@@ -123,27 +180,6 @@ void GazeboRosDiffdrive::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf )
   if (_sdf->HasElement("max_velocity"))
     max_velocity_ = _sdf->GetElement("max_velocity")->Get<double>();
 
-
-  if (!ros::isInitialized())
-  {
-    int argc = 0;
-    char** argv = NULL;
-    ros::init(argc, argv, "gazebo_ros_diffdrive_uos", ros::init_options::NoSigintHandler|ros::init_options::AnonymousName);
-  }
-
-  rosnode_ = new ros::NodeHandle(node_namespace_);
-
-  cmd_vel_sub_ = rosnode_->subscribe(cmd_vel_topic_name_, 1, &GazeboRosDiffdrive::OnCmdVel, this);
-  odom_pub_ = rosnode_->advertise<nav_msgs::Odometry> (odom_topic_name_, 1);
-  joint_state_pub_ = rosnode_->advertise<sensor_msgs::JointState> (joint_states_topic_name_, 1);
-
-  for (size_t i = 0; i < NUM_JOINTS; ++i)
-  {
-    joints_[i] = my_parent_->GetJoint(js_.name[i]);
-    if (!joints_[i])
-      gzthrow("The controller couldn't get joint " << js_.name[i]);
-  }
-
   //initialize time and odometry position
   prev_update_time_ = last_cmd_vel_time_ = this->my_world_->GetSimTime();
   odom_pose_[0] = 0.0;
@@ -159,7 +195,7 @@ void GazeboRosDiffdrive::Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf )
       boost::bind(&GazeboRosDiffdrive::UpdateChild, this));
   gzdbg << "plugin model name: " << modelName << "\n";
 
-  ROS_INFO("gazebo_ros_diffdrive_uos plugin initialized");
+  ROS_INFO_STREAM("Plugin gazebo_ros_diffdrive_uos_uos initialized.");
 }
 
 void GazeboRosDiffdrive::UpdateChild()
@@ -180,18 +216,18 @@ void GazeboRosDiffdrive::UpdateChild()
   d1 = d2 = 0;
   dr = da = 0;
 
-  // Distance travelled by middle wheels
-  d1 = step_time.Double() * (wd / 2) * joints_[LEFT]->GetVelocity(0);
-  d2 = step_time.Double() * (wd / 2) * joints_[RIGHT]->GetVelocity(0);
+  // Distance travelled by middle (six wheeled robot) or by front wheels (four wheeled robot)
+  d1 = step_time.Double() * (wd / 2) * joints_[left]->GetVelocity(0);
+  d2 = step_time.Double() * (wd / 2) * joints_[right]->GetVelocity(0);
 
   // Can see NaN values here, just zero them out if needed
   if (std::isnan(d1)) {
-    ROS_WARN_THROTTLE(0.1, "gazebo_ros_diffdrive_uos: NaN in d1. Step time: %.2f. WD: %.2f. Velocity: %.2f", step_time.Double(), wd, joints_[LEFT]->GetVelocity(0));
+    ROS_WARN_THROTTLE(0.1, "gazebo_ros_diffdrive_uos: NaN in d1. Step time: %.2f. WD: %.2f. Velocity: %.2f", step_time.Double(), wd, joints_[left]->GetVelocity(0));
     d1 = 0;
   }
 
   if (std::isnan(d2)) {
-    ROS_WARN_THROTTLE(0.1, "gazebo_ros_diffdrive_uos: NaN in d2. Step time: %.2f. WD: %.2f. Velocity: %.2f", step_time.Double(), wd, joints_[RIGHT]->GetVelocity(0));
+    ROS_WARN_THROTTLE(0.1, "gazebo_ros_diffdrive_uos: NaN in d2. Step time: %.2f. WD: %.2f. Velocity: %.2f", step_time.Double(), wd, joints_[right]->GetVelocity(0));
     d2 = 0;
   }
 
@@ -210,14 +246,14 @@ void GazeboRosDiffdrive::UpdateChild()
 
   if (this->my_world_->GetSimTime() > last_cmd_vel_time_ + common::Time(CMD_VEL_TIMEOUT))
   {
-	ROS_DEBUG("gazebo_ros_diffdrive_uos: cmd_vel timeout - current: %f, last cmd_vel: %f, timeout: %f", this->my_world_->GetSimTime().Double(), last_cmd_vel_time_.Double(), common::Time(CMD_VEL_TIMEOUT).Double());
-	wheel_speed_left_ = wheel_speed_right_ = 0.0;
+    ROS_DEBUG("gazebo_ros_diffdrive_uos: cmd_vel timeout - current: %f, last cmd_vel: %f, timeout: %f", this->my_world_->GetSimTime().Double(), last_cmd_vel_time_.Double(), common::Time(CMD_VEL_TIMEOUT).Double());
+    wheel_speed_left_ = wheel_speed_right_ = 0.0;
   }
 
   ROS_DEBUG("gazebo_ros_diffdrive_uos: setting wheel speeds (left; %f, right: %f)", wheel_speed_left_ / (wd / 2.0), wheel_speed_right_ / (wd / 2.0));
 
   // turn left wheels
-  for (unsigned short i = 0; i < NUM_JOINTS/2; i++)
+  for (unsigned short i = 0; i < num_joints_/2; i++)
   {
     joints_[i]->SetVelocity(0, wheel_speed_left_ / (wd / 2.0));
 #if GAZEBO_MAJOR_VERSION < 5
@@ -226,7 +262,7 @@ void GazeboRosDiffdrive::UpdateChild()
   }
 
   // turn right wheels
-  for (unsigned short i = NUM_JOINTS/2; i < NUM_JOINTS; i++)
+  for (unsigned short i = num_joints_/2; i < num_joints_; i++)
   {
     joints_[i]->SetVelocity(0, wheel_speed_right_ / (wd / 2.0));
 #if GAZEBO_MAJOR_VERSION < 5
@@ -274,7 +310,7 @@ void GazeboRosDiffdrive::UpdateChild()
   js_.header.stamp.sec = time_now.sec;
   js_.header.stamp.nsec = time_now.nsec;
 
-  for (size_t i = 0; i < NUM_JOINTS; ++i)
+  for (size_t i = 0; i < num_joints_; ++i)
   {
     js_.position[i] = joints_[i]->GetAngle(0).Radian();
     js_.velocity[i] = joints_[i]->GetVelocity(0);
